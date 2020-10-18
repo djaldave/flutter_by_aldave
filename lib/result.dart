8aea16e9e1cd3a87e10d9e2f959ddf9926934c83
djaldave <djaldave@gmail.com>
2020-10-19 02:15:30 +0800

    getters anf else if

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  Result(this.resultScore);
+  String get resultPhrase {
+    var resultText = "you did it";
+    if (resultScore <= 10) {
+      resultText = "you are awesome";
+    } else if (resultScore <= 12) {
+      resultText = "pretty likeable";
+    } else if (resultScore <= 16) {
+      resultText = "strange?";
+    } else {
+      resultText = "you are so cool";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You did it"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
