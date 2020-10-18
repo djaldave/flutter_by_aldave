97349f3b4bf0f5eb7d54ba35e45756e8beed8c09
djaldave <djaldave@gmail.com>
2020-10-18 16:48:41 +0800

    building and app from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Al Dave"),
+    );
+  }
+}
