5502e6ded5ab8fd8a42ce04e876bf57a7cab88d2
djaldave <djaldave@gmail.com>
2020-10-19 02:23:05 +0800

    resetting the quiz

--- lib/result.dart
+++ lib/result.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
-  Result(this.resultScore);
+  final Function restHandler;
+
+  Result(this.resultScore, this.restHandler);
   String get resultPhrase {
     var resultText = "you did it";
     if (resultScore <= 10) {
@@ -20,13 +22,22 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: restHandler,
+            child: Text("Restart Quiz!"),
+            textColor: Colors.blue,
+          )
+        ],
       ),
     );
   }
