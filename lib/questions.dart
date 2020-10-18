a131eaafebc0968514c16b7fbf607de1f3c3e28f
djaldave <djaldave@gmail.com>
2020-10-18 17:46:00 +0800

    first styling and layouting steps

--- lib/questions.dart
+++ lib/questions.dart
@@ -6,6 +6,16 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(top: 15),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
