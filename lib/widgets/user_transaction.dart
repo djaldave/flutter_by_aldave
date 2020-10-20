568d541bb572b0e4585eecb17c3ad908677f60ca
djaldave <djaldave@gmail.com>
2020-10-20 12:53:32 +0800

    splitting the app into widgets [unfinish]

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+import 'new_transaction.dart';
+import 'transaction_list.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+      id: "1",
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "2",
+      title: "Groceries",
+      amount: 16.54,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        NewTransaction(),
+        TransactionList(),
+      ],
+    );
+  }
+}
