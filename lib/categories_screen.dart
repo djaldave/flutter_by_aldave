fa146e774344d6d9bf2a9266db186e16490b5b93
djaldave <djaldave@gmail.com>
2020-10-24 00:42:14 +0800

    starting with the app - adding base data

---
+++ lib/categories_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class CategoriesScreen extends StatelessWidget {
+	@override
+	Widget build(context){
+		return GridView(
+			children: [
+				
+			],
+			gridDelegate: 
+			SliverGridDelegateWithMaxCrossAxisExtent(
+				maxCrossAxisExtent: 200,
+				childAspectRatio: 3/ 2,
+				crossAxisSpacing: 20,
+				mainAxisSpacing: 20,
+			),
+		);
+	}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,36 @@
+import "package:flutter/material.dart";
 
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context){
+    return MaterialApp(
+      title: 'Meal App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(context)  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meal App"),
+      ),
+      body: Center(
+        child: Text("Navigation time"),
+      ),
+    );
+  }
+}
