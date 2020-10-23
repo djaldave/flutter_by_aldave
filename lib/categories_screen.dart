f28828753ee683f8b38642519d678aa88a13b0f1
djaldave <djaldave@gmail.com>
2020-10-24 02:12:41 +0800

    creating a grid and working with linear gradient

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
+import 'category_item.dart';
 
 
 class CategoriesScreen extends StatelessWidget {
 	@override
 	Widget build(context){
 		return GridView(
-			children: [
-				
-			],
+			children: DUMMY_CATEGORIES
+			.map((catData) => CategoryItem(
+				catData.title, catData.color,
+				),).toList(),
 			gridDelegate: 
 			SliverGridDelegateWithMaxCrossAxisExtent(
 				maxCrossAxisExtent: 200,
