3929c77ba5562788d39d4b014ef6772ef03d0895
djaldave <djaldave@gmail.com>
2020-12-06 12:23:31 +0800

    exploring alternative provider Syntaxes

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ void main() => runApp(Main());
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return ChangeNotifierProvider.value(
+      value: Products(),
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
