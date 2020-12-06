be2b43c28f4bbb6708f06bdc2def210b9a4c4f3a
djaldave <djaldave@gmail.com>
2020-12-06 09:48:10 +0800

    let's build a shop

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyShop"),
+      ),
+      body: Center(
+        child: Text("Let's build a shop"),
+      ),
+    );
+  }
+}
