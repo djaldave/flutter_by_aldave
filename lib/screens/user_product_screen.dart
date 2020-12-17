4a2d1ac6bd82e2c4c1e09a73c1ed13041f6fcf6e
djaldave <djaldave@gmail.com>
2020-12-18 00:29:19 +0800

    time to update products

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -32,6 +32,7 @@ class UserProductScreen extends StatelessWidget {
           itemBuilder: (ctx, i) => Column(
             children: [
               UserProductItem(
+                productsData.items[i].id,
                 productsData.items[i].title,
                 productsData.items[i].imageUrl,
               ),
