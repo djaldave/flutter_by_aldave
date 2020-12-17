6745196cafd56cbc1e52a13c5327c9e49a994328
djaldave <djaldave@gmail.com>
2020-12-17 16:45:13 +0800

    edit product screen and using form & working with form inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'screens/cart_screen.dart';
-import 'screens/products_overview_screen.dart';
-import 'screens/product_detail_screen.dart';
-import 'providers/products.dart';
 import 'package:provider/provider.dart';
+
 import 'providers/cart.dart';
 import 'providers/orders.dart';
+import 'providers/products.dart';
+import 'screens/cart_screen.dart';
+import 'screens/edit_product_screen.dart';
 import 'screens/orders_screen.dart';
+import 'screens/product_detail_screen.dart';
+import 'screens/products_overview_screen.dart';
 import 'screens/user_product_screen.dart';
 
 void main() => runApp(Main());
@@ -38,6 +40,7 @@ class Main extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductScreen.routeName: (ctx) => UserProductScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import '../providers/products.dart';
 import 'package:provider/provider.dart';
-import '../widgets/user_product_item.dart';
+
+import '../providers/products.dart';
+import '../screens/edit_product_screen.dart';
 import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
 
 class UserProductScreen extends StatelessWidget {
   static const routeName = "/user-product";
@@ -14,7 +16,11 @@ class UserProductScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                EditProductScreen.routeName,
+              );
+            },
           ),
         ],
         title: const Text("Your Products"),
