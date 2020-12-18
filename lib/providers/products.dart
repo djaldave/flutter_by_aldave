7edce1e4f45a84e637e258444e128475a5da6882
djaldave <djaldave@gmail.com>
2020-12-19 03:42:59 +0800

    working with async and await

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -66,19 +66,18 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     const url =
         "https://flutter-update-4e157-default-rtdb.firebaseio.com/products.json";
-    return http
-        .post(url,
-            body: json.encode({
-              'title': product.title,
-              'description': product.description,
-              'imageUrl': product.imageUrl,
-              'price': product.price,
-              'isFavorite': product.isFavorite,
-            }))
-        .then((response) {
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'title': product.title,
+            'description': product.description,
+            'imageUrl': product.imageUrl,
+            'price': product.price,
+            'isFavorite': product.isFavorite,
+          }));
       // print(json.decode(response.body));
       final newProduct = Product(
         title: product.title,
@@ -90,10 +89,10 @@ class Products with ChangeNotifier {
       _items.add(newProduct);
       // _items.insert(0, newProduct); // at the start of the list
       notifyListeners();
-    }).catchError((error) {
+    } catch (error) {
       print(error);
       throw error;
-    });
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
